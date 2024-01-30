85e07eb03c62937d63193c7d3402ea4926e182e0
unknown <araf.chakib.m@gmail.com>
2024-01-30 23:24:53 +0100

    fonctionnalité ajout de la photo

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import { useState } from 'react';
+const PlaceholderImage = require('./assets/images/soleil.jpg');
+import ImageViewer from './components/imageViewer';
+import Button from './components/button';
+import * as ImagePicker from 'expo-image-picker';
+
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('aucune image selectionné')
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} 
+        selectedImage={selectedImage}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="choisir photo" onPress={pickImageAsync}></Button>
+        <Button label={"utiliser cette photo"}></Button>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+
+  },
+
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+
   },
 });
